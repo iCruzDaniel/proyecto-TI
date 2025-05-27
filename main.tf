e6879dbeddb54bcf1b08278cbaa0a35356dd10ce
iCruzDaniel <sp_dicruz044@hotmail.com>
2025-05-27 17:48:06 -0500

    cambio de puerto

--- main.tf
+++ main.tf
@@ -62,7 +62,7 @@ variable "secret_key" {
 resource "digitalocean_vpc" "main" {
   name     = "vpc-main2"
   region   = "nyc3"
-  ip_range = "10.10.0.0/16"
+  ip_range = "10.10.0.0/21"
 }
 
 # Base de datos MySQL
